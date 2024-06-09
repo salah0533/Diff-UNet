aa4142a88144e9217336413e14b183136ba52893
salah0533 <sallahpsg533@gmail.com>
2024-06-09 23:23:48 +0100

    .

--- BraTS2020/train.py
+++ BraTS2020/train.py
@@ -28,7 +28,7 @@ kaggle_dir = '/kaggle/working/logs_brats/diffusion_seg_all_loss_embed/model'
 
 #env = "DDP" # or env = "pytorch" if you only have one gpu.
 env = "pytorch" # or env = "pytorch" if you only have one gpu.
-max_epoch = 200
+max_epoch = 1
 batch_size = 1
 val_every =1
 num_gpus = 1
